e44ac3be99af5bf7424464e5112ffe701120fb74
Franze, Mario <mfranze@germandev.de>
2019-08-30 18:19:29 +0200

    * Switched to new route handling

--- server/routes/account/login.js
+++ server/routes/account/login.js
@@ -1,33 +1,32 @@
 // const bcrypt = require('bcryptjs');
-const express = require('express');
-const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
 
-router.post('/account/login',
-	[
-		check('username')
-			.not().isEmpty()
-			.isLength({ min: 5 })
-			.trim()
-			.escape(),
-		check('password')
-			.not().isEmpty()
-			.isLength({ min: 8 }),
-	], (req, res) =>
-	{
-		const errors = validationResult(req);
-		if(!errors.isEmpty())
+module.exports = (app, db) =>
+{
+	app.post('/account/login',
+		[
+			check('username')
+				.not().isEmpty()
+				.isLength({ min: 5 })
+				.trim()
+				.escape(),
+			check('password')
+				.not().isEmpty()
+				.isLength({ min: 8 }),
+		], (req, res) =>
 		{
-			return res.status(403).json(
-				{
-					'Error':
+			const errors = validationResult(req);
+			if(!errors.isEmpty())
+			{
+				return res.status(403).json(
+					{
+						'Error':
 					{
 						'Code': 403,
 						'Messages': errors.array(),
 					},
-				});
+					});
+			}
 		}
-	}
-);
-
-module.exports = router;
+	);
+};
--- server/routes/account/register.js
+++ server/routes/account/register.js
@@ -1,48 +1,47 @@
 // const bcrypt = require('bcryptjs');
-const express = require('express');
-const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
 
-router.post('/account/register',
-	[
-		check('username')
-			.not().isEmpty()
-			.isLength({ min: 5 })
-			.trim()
-			.escape(),
-		check('password')
-			.not().isEmpty()
-			.isLength({ min: 8 }),
-		check('password-retype')
-			.not().isEmpty()
-			.isLength({ min: 8 })
-			.custom((value, { req }) =>
-			{
-				if (value !== req.body.password)
+module.exports = (app, db) =>
+{
+	app.post('/account/register',
+		[
+			check('username')
+				.not().isEmpty()
+				.isLength({ min: 5 })
+				.trim()
+				.escape(),
+			check('password')
+				.not().isEmpty()
+				.isLength({ min: 8 }),
+			check('password-retype')
+				.not().isEmpty()
+				.isLength({ min: 8 })
+				.custom((value, { req }) =>
 				{
-					// trow error if passwords do not match
-					throw new Error('The given passwords don\'t match');
-				}
-				else
-				{
-					return value;
-				}
-			}),
-	], (req, res) =>
-	{
-		const errors = validationResult(req);
-		if(!errors.isEmpty())
-		{
-			return res.status(400).json(
-				{
-					'Error':
+					if (value !== req.body.password)
 					{
-						'Code': 400,
-						'Messages': errors.array(),
-					},
-				});
+						// trow error if passwords do not match
+						throw new Error('The given passwords don\'t match');
+					}
+					else
+					{
+						return value;
+					}
+				}),
+		], (req, res) =>
+		{
+			const errors = validationResult(req);
+			if(!errors.isEmpty())
+			{
+				return res.status(400).json(
+					{
+						'Error':
+						{
+							'Code': 400,
+							'Messages': errors.array(),
+						},
+					});
+			}
 		}
-	}
-);
-
-module.exports = router;
+	);
+};
