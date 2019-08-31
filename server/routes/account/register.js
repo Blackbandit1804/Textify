bc3fde97a464058c8c8ddfbebb2b4254fe1c3761
Franze, Mario <mfranze@germandev.de>
2019-08-31 06:41:00 +0200

    * Finished registration

--- server/routes/account/register.js
+++ server/routes/account/register.js
@@ -1,4 +1,4 @@
-// const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator/check');
 
 module.exports = (app, db) =>
@@ -42,6 +42,93 @@ module.exports = (app, db) =>
 						},
 					});
 			}
+			db.account.findOne({ where: { username: req.body.username } })
+				.then(exist =>
+				{
+					if(exist)
+					{
+						return res.status(400).json(
+							{
+								'Error':
+								{
+									'Code': 400,
+									'Messages': [
+										{
+											'msg': 'The given username allready exists',
+										},
+									],
+								},
+							});
+					}
+					bcrypt.genSalt(parseInt(process.env.PASSWORD_SALT_ROUNDS), (err, salt) =>
+					{
+						if(err)
+						{
+							return res.status(500).json(
+								{
+									'Error':
+									{
+										'Code': 500,
+										'Messages': [
+											{
+												'msg': err.message,
+											},
+										],
+									},
+								});
+						}
+						bcrypt.hash(req.body.password, salt, (err, hash) =>
+						{
+							if(err)
+							{
+								return res.status(500).json(
+									{
+										'Error':
+										{
+											'Code': 500,
+											'Messages': [
+												{
+													'msg': err.message,
+												},
+											],
+										},
+									});
+							}
+							db.account.create(
+								{
+									username: req.body.username,
+									password: hash,
+								})
+								.then(acc =>
+								{
+									acc.password = '**********';
+									res.status(200).json(
+										{
+											'Success':
+											{
+												'Code': 200,
+												'Data': acc,
+											},
+										});
+								})
+								.catch(err =>
+								{
+									res.status(500).json(
+										{
+											'Error':
+											{
+												'Code': 500,
+												'Messages': [
+													{
+														'msg': err.message,
+													},
+												],
+											},
+										});
+								});
+						});
+					});
+				});
 		}
 	);
 };
