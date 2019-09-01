3d44f9625f96231b025595e54da40235da8e8d11
Franze, Mario <mfranze@codingden.de>
2019-09-01 09:31:29 +0200

    + Added some login logic

--- server/routes/account/login.js
+++ server/routes/account/login.js
@@ -1,4 +1,4 @@
-// const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator/check');
 
 module.exports = (app, db) =>
@@ -18,15 +18,66 @@ module.exports = (app, db) =>
 			const errors = validationResult(req);
 			if(!errors.isEmpty())
 			{
-				return res.status(403).json(
+				return res.status(400).json(
 					{
 						'Error':
 					{
-						'Code': 403,
+						'Code': 400,
 						'Messages': errors.array(),
 					},
 					});
 			}
-		}
-	);
+			db.account.findOne({ where: { username: req.body.username } })
+				.then(usr_acc =>
+				{
+					if(!usr_acc)
+					{
+						return res.status(403).json(
+							{
+								'Error':
+								{
+									'Code': 403,
+									'Messages': [
+										{
+											'msg': 'The given username does not exists',
+										},
+									],
+								},
+							});
+					}
+					bcrypt.compare(req.body.password, usr_acc.password, (err, match) =>
+					{
+						if(err)
+						{
+							return res.status(500).json(
+								{
+									'Error':
+									{
+										'Code': 500,
+										'Messages': [
+											{
+												'msg': err.message,
+											},
+										],
+									},
+								});
+						}
+						if(!match)
+						{
+							return res.status(403).json(
+								{
+									'Error':
+									{
+										'Code': 403,
+										'Messages': [
+											{
+												'msg': 'The given password is not correct',
+											},
+										],
+									},
+								});
+						}
+					});
+				});
+		});
 };
