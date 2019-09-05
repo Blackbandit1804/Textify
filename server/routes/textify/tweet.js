8d90107841e9155e172eafdc81583f5bc7013741
Franze, Mario <mfranze@germandev.de>
2019-09-05 07:12:54 +0200

    + Added display of a sigle tweet

--- server/routes/textify/tweet.js
+++ server/routes/textify/tweet.js
@@ -81,7 +81,7 @@ module.exports = (app, db) =>
 		});
 	// #endregion
 
-	// #region GetTweetsByUsername
+	// #region Show user tweets
 	app.post('/u/:username?',
 		[
 			check('token')
@@ -194,6 +194,111 @@ module.exports = (app, db) =>
 														'Data': tweets,
 													},
 												});
+										});
+								});
+						});
+				});
+		});
+	// #endregion
+
+	// #region Show tweet
+	app.post('/t/:tweetId?',
+		[
+			check('token')
+				.not().isEmpty()
+				.trim()
+				.escape(),
+		], (req, res) =>
+		{
+			const errors = validationResult(req);
+			if(!errors.isEmpty())
+			{
+				return res.status(400).json(
+					{
+						'Error':
+					{
+						'Code': 400,
+						'Messages': errors.array(),
+					},
+					});
+			}
+			db.sessions.findOne(
+				{
+					where:
+				{
+					token: req.body.token,
+				},
+				})
+				.then(sessionData =>
+				{
+					if(!sessionData)
+					{
+						return res.status(403).json(
+							{
+								'Error':
+							{
+								'Code': 403,
+								'Messages': [
+									{
+										'msg': 'Incorrect token',
+									},
+								],
+							},
+							});
+					}
+					const validUntil = new Date(Date.now() + (30 * 60 * 1000) + (2 * 60 * 60 * 1000));
+					sessionData.update(
+						{
+							validUntil: validUntil,
+						})
+						.then(() =>
+						{
+							if(!req.params.tweetId || !parseInt(req.params.tweetId))
+							{
+								return res.status(400).json(
+									{
+										'Error':
+									{
+										'Code': 400,
+										'Messages': [
+											{
+												'msg': 'Missing or wrong parameter tweetId',
+											},
+										],
+									},
+									});
+							}
+							db.tweet.findOne(
+								{
+									where:
+									{
+										id: req.params.tweetId,
+									},
+								})
+								.then(tweetMessage =>
+								{
+									if(!tweetMessage)
+									{
+										return res.status(404).json(
+											{
+												'Error':
+											{
+												'Code': 404,
+												'Messages': [
+													{
+														'msg': 'Tweet not found',
+													},
+												],
+											},
+											});
+									}
+									res.status(200).json(
+										{
+											'Success':
+											{
+												'Code': 200,
+												'Data': tweetMessage,
+											},
 										});
 								});
 						});
