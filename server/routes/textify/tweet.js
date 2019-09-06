9faacb0f33364d2cf3b22b0dd58e3a7f8fceb358
Franze, Mario <mfranze@germandev.de>
2019-09-06 06:59:02 +0200

    * changed from spaghetti code to own session manager

--- server/routes/textify/tweet.js
+++ server/routes/textify/tweet.js
@@ -1,4 +1,5 @@
 const { check, validationResult } = require('express-validator/check');
+const SessionManager = require('../../utils/sessionManager');
 
 module.exports = (app, db) =>
 {
@@ -12,7 +13,7 @@ module.exports = (app, db) =>
 			check('message')
 				.not().isEmpty()
 				.isLength({ min: 2 }),
-		], (req, res) =>
+		], async (req, res) =>
 		{
 			const errors = validationResult(req);
 			if(!errors.isEmpty())
@@ -26,56 +27,54 @@ module.exports = (app, db) =>
 						},
 					});
 			}
-			db.sessions.findOne(
-				{
-					where:
+			const sessionManager = new SessionManager(db, req.body.token);
+			if(await !sessionManager.checkSession())
+			{
+				return res.status(403).json(
 					{
-						token: req.body.token,
-					},
-				})
-				.then(sessionData =>
-				{
-					if(!sessionData)
-					{
-						return res.status(403).json(
-							{
-								'Error':
+						'Error':
+						{
+							'Code': 403,
+							'Messages': [
 								{
-									'Code': 403,
-									'Messages': [
-										{
-											'msg': 'Incorrect token',
-										},
-									],
+									'msg': 'Incorrect token',
 								},
-							});
-					}
-					const validUntil = new Date(Date.now() + (30 * 60 * 1000) + (2 * 60 * 60 * 1000));
-					sessionData.update(
+							],
+						},
+					});
+			}
+			if(await !sessionManager.updateSession)
+			{
+				return res.status(500).json(
+					{
+						'Error':
 						{
-							validUntil: validUntil,
-						})
-						.then(() =>
+							'Code': 500,
+							'Messages': [
+								{
+									'msg': 'Internal server error',
+								},
+							],
+						},
+					});
+			}
+			db.tweet.create(
+				{
+					text: req.body.message,
+					accountId: sessionManager.getCurrentSessionObject().accountId,
+				})
+				.then(tweet =>
+				{
+					delete tweet.dataValues['id'];
+					delete tweet.dataValues['accountId'];
+					delete tweet.dataValues['updatedAt'];
+					res.status(200).json(
 						{
-							db.tweet.create(
-								{
-									text: req.body.message,
-									accountId: sessionData.accountId,
-								})
-								.then(tweet =>
-								{
-									delete tweet.dataValues['id'];
-									delete tweet.dataValues['accountId'];
-									delete tweet.dataValues['updatedAt'];
-									res.status(200).json(
-										{
-											'Success':
-											{
-												'Code': 200,
-												'Data': tweet,
-											},
-										});
-								});
+							'Success':
+							{
+								'Code': 200,
+								'Data': tweet,
+							},
 						});
 				});
 		});
@@ -88,7 +87,7 @@ module.exports = (app, db) =>
 				.not().isEmpty()
 				.trim()
 				.escape(),
-		], (req, res) =>
+		], async (req, res) =>
 		{
 			const errors = validationResult(req);
 			if(!errors.isEmpty())
@@ -102,99 +101,99 @@ module.exports = (app, db) =>
 					},
 					});
 			}
-			db.sessions.findOne(
+			const sessionManager = new SessionManager(db, req.body.token);
+			if(await !sessionManager.checkSession())
+			{
+				return res.status(403).json(
+					{
+						'Error':
+						{
+							'Code': 403,
+							'Messages': [
+								{
+									'msg': 'Incorrect token',
+								},
+							],
+						},
+					});
+			}
+			if(await !sessionManager.updateSession)
+			{
+				return res.status(500).json(
+					{
+						'Error':
+						{
+							'Code': 500,
+							'Messages': [
+								{
+									'msg': 'Internal server error',
+								},
+							],
+						},
+					});
+			}
+
+
+			if(!req.params.username)
+			{
+				return res.status(400).json(
+					{
+						'Error':
+					{
+						'Code': 400,
+						'Messages': [
+							{
+								'msg': 'Missing parameter username',
+							},
+						],
+					},
+					});
+			}
+			db.account.findOne(
 				{
 					where:
 				{
-					token: req.body.token,
+					username: req.params.username,
 				},
+					attributes: ['id'],
 				})
-				.then(sessionData =>
+				.then(account =>
 				{
-					if(!sessionData)
+					if(!account)
 					{
-						return res.status(403).json(
+						return res.status(400).json(
 							{
 								'Error':
 							{
-								'Code': 403,
+								'Code': 400,
 								'Messages': [
 									{
-										'msg': 'Incorrect token',
+										'msg': 'Account not found',
 									},
 								],
 							},
 							});
 					}
-					const validUntil = new Date(Date.now() + (30 * 60 * 1000) + (2 * 60 * 60 * 1000));
-					sessionData.update(
+					db.tweet.findAll(
 						{
-							validUntil: validUntil,
-						})
-						.then(() =>
-						{
-							if(!req.params.username)
+							where:
 							{
-								return res.status(400).json(
+								accountId: account.id,
+							},
+							order:
+							[
+								[ 'id', 'DESC' ],
+							],
+						})
+						.then(tweets =>
+						{
+							res.status(200).json(
+								{
+									'Success':
 									{
-										'Error':
-									{
-										'Code': 400,
-										'Messages': [
-											{
-												'msg': 'Missing parameter username',
-											},
-										],
+										'Code': 200,
+										'Data': tweets,
 									},
-									});
-							}
-							db.account.findOne(
-								{
-									where:
-								{
-									username: req.params.username,
-								},
-									attributes: ['id'],
-								})
-								.then(account =>
-								{
-									if(!account)
-									{
-										return res.status(400).json(
-											{
-												'Error':
-											{
-												'Code': 400,
-												'Messages': [
-													{
-														'msg': 'Account not found',
-													},
-												],
-											},
-											});
-									}
-									db.tweet.findAll(
-										{
-											where:
-											{
-												accountId: account.id,
-											},
-											order:
-											[
-												[ 'id', 'DESC' ],
-											],
-										})
-										.then(tweets =>
-										{
-											res.status(200).json(
-												{
-													'Success':
-													{
-														'Code': 200,
-														'Data': tweets,
-													},
-												});
-										});
 								});
 						});
 				});
@@ -208,7 +207,7 @@ module.exports = (app, db) =>
 				.not().isEmpty()
 				.trim()
 				.escape(),
-		], (req, res) =>
+		], async (req, res) =>
 		{
 			const errors = validationResult(req);
 			if(!errors.isEmpty())
@@ -222,85 +221,68 @@ module.exports = (app, db) =>
 					},
 					});
 			}
-			db.sessions.findOne(
+			const sessionManager = new SessionManager(db, req.body.token);
+			if(await !sessionManager.checkSession())
+			{
+				return res.status(403).json(
+					{
+						'Error':
+						{
+							'Code': 403,
+							'Messages': [
+								{
+									'msg': 'Incorrect token',
+								},
+							],
+						},
+					});
+			}
+			if(await !sessionManager.updateSession)
+			{
+				return res.status(500).json(
+					{
+						'Error':
+						{
+							'Code': 500,
+							'Messages': [
+								{
+									'msg': 'Internal server error',
+								},
+							],
+						},
+					});
+			}
+			db.tweet.findOne(
 				{
 					where:
-				{
-					token: req.body.token,
-				},
-				})
-				.then(sessionData =>
-				{
-					if(!sessionData)
 					{
-						return res.status(403).json(
+						id: req.params.tweetId,
+					},
+				})
+				.then(tweetMessage =>
+				{
+					if(!tweetMessage)
+					{
+						return res.status(404).json(
 							{
 								'Error':
 							{
-								'Code': 403,
+								'Code': 404,
 								'Messages': [
 									{
-										'msg': 'Incorrect token',
+										'msg': 'Tweet not found',
 									},
 								],
 							},
 							});
 					}
-					const validUntil = new Date(Date.now() + (30 * 60 * 1000) + (2 * 60 * 60 * 1000));
-					sessionData.update(
+					res.status(200).json(
 						{
-							validUntil: validUntil,
-						})
-						.then(() =>
-						{
-							if(!req.params.tweetId || !parseInt(req.params.tweetId))
+							'Success':
 							{
-								return res.status(400).json(
-									{
-										'Error':
-									{
-										'Code': 400,
-										'Messages': [
-											{
-												'msg': 'Missing or wrong parameter tweetId',
-											},
-										],
-									},
-									});
-							}
-							db.tweet.findOne(
-								{
-									where:
-									{
-										id: req.params.tweetId,
-									},
-								})
-								.then(tweetMessage =>
-								{
-									if(!tweetMessage)
-									{
-										return res.status(404).json(
-											{
-												'Error':
-											{
-												'Code': 404,
-												'Messages': [
-													{
-														'msg': 'Tweet not found',
-													},
-												],
-											},
-											});
-									}
-									res.status(200).json(
-										{
-											'Success':
-											{
-												'Code': 200,
-												'Data': tweetMessage,
-											},
-										});
-								});
+								'Code': 200,
+								'Data': tweetMessage,
+							},
 						});
 				});
 		});
